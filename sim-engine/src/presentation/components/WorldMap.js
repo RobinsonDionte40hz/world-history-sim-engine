101117119cbcaebd70e09fa15a9e4c63c1d3c3fe
RobinsonDionte40hz <dionterobinson.bioRxiv@gmail.com>
2025-07-11 05:53:47 -0500

    Add WorldMap component to visualize simulation nodes and NPCs

---
+++ sim-engine/src/presentation/components/WorldMap.js
@@ -0,0 +1,21 @@
+// src/presentation/components/features/WorldMap.js
+
+import React from 'react';
+import SimulationService from '../../../application/services/SimulationService.js';
+
+const WorldMap = () => {
+  const worldState = SimulationService.worldState || { nodes: [], npcs: [] };
+
+  return (
+    <svg width="400" height="400" className="border">
+      {worldState.nodes.map(node => (
+        <circle key={node.id} cx={node.position.x * 40} cy={node.position.y * 40} r="10" fill="blue" />
+      ))}
+      {worldState.npcs.map(npc => (
+        <circle key={npc.id} cx={npc.position.x * 40} cy={npc.position.y * 40} r="5" fill="red" />
+      ))}
+    </svg>
+  );
+};
+
+export default WorldMap;
